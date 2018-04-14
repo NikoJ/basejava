ca5ed84bacc83855eb89e0cb2b6285ae098f3350
Nick_Jv <rnjv2015@yandex.ru>
2018-04-14 05:56:35 +0300

    Task completed! Updated methods of save,get,delete,size,clear,getAll.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,108 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    /**
+     * Equate all values of Resume to null
+     */
     void clear() {
+        for (int i =0; i< storage.length;i++ )
+        {
+            storage[i]=null;
+        }
     }
 
+    /**
+     *
+     * @param r - object Resume to be saved
+     */
     void save(Resume r) {
+
+        for (int i =0; i< storage.length;i++ )
+        {
+            if (storage[i] == null)
+            {
+                storage[i]=r;
+                break;
+            }
+        }
     }
 
+    /**
+     *
+     * @param uuid - Unique identifier in Resume
+     * @return
+     */
     Resume get(String uuid) {
+
+        for (int i =0; i< size();i++ )
+        {
+            if (storage[i].uuid == uuid)
+            {
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
+    /**
+     *
+     * @param uuid - Unique identifier in Resume
+     */
     void delete(String uuid) {
+
+        for (int i =0; i< size();i++ )
+        {
+            if (storage[i].uuid == uuid)
+            {
+                storage[i]=null;
+                while (i<size()){
+                    storage[i]=storage[i+1];
+                    i++;
+                }
+
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        List<Resume> list = new ArrayList<>();
+        for (int i =0; i<size();i++ )
+        {
+            list.add(storage[i]);
+        }
+
+        Resume[] temp = new Resume[list.size()];
+
+        list.toArray(temp);
+
+        return temp;
     }
 
+    /**
+     *
+     * @return size array, contains only Resumes in storage (without null)
+     */
     int size() {
-        return 0;
+
+        int size=0;
+
+        for (int i =0; i< storage.length;i++ )
+        {
+            if (storage[i] != null) size++;
+        }
+
+        return size;
     }
 }
