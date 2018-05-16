807dfb66a682a89b3e1836e20f3e2380b415b4a6
Nick_Jv <rnjv2015@yandex.ru>
2018-05-17 00:53:34 +0300

    added Enum (lesson 7.4)

--- src/com/urise/webapp/TestSingleton.java
+++ src/com/urise/webapp/TestSingleton.java
@@ -9,4 +9,14 @@ public class TestSingleton {
     public static TestSingleton getInstance() {
         return instance == null ? instance = new TestSingleton() : instance;
     }
+
+    public static void main(String[] args) {
+        TestSingleton.getInstance().toString();
+        Singleton singleton = Singleton.valueOf("INSTANCE");
+        System.out.println(singleton.ordinal());
+    }
+
+    public enum Singleton{
+        INSTANCE
+    }
 }
