8c21213696059e6d883e8e1acd1d054bc752fd5e
Nick_Jv <rnjv2015@yandex.ru>
2018-07-07 19:34:32 +0300

    HW_10

--- src/com/urise/webapp/storage/serializer/DataSerializerStrategy.java
+++ src/com/urise/webapp/storage/serializer/DataSerializerStrategy.java
@@ -1,9 +1,11 @@
 package com.urise.webapp.storage.serializer;
 
-import com.urise.webapp.model.ContactType;
-import com.urise.webapp.model.Resume;
+import com.urise.webapp.model.*;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DataSerializerStrategy implements SerializerStrategy {
@@ -18,7 +20,46 @@ public class DataSerializerStrategy implements SerializerStrategy {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             }
-            //TODO implements Sections
+
+            Map<SectionType, Section> sections = resume.getSections();
+            dos.writeInt(sections.size());
+            for (Map.Entry<SectionType, Section> entry : resume.getSections().entrySet()) {
+                SectionType sectionType = entry.getKey();
+                Section section = entry.getValue();
+                dos.writeUTF(sectionType.name());
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        dos.writeUTF(((TextBoxSection) section).getText());
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        List<String> listSectionList = (((ListSection) section).getList());
+                        dos.writeInt(listSectionList.size());
+                        for (String listSection : listSectionList) {
+                            dos.writeUTF(listSection);
+                        }
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        List<Company> companyList = (((CompanySection) section).getCompanies());
+                        dos.writeInt(companyList.size());
+                        for (Company company : companyList) {
+                            dos.writeUTF(company.getSitePage().getName());
+                            dos.writeUTF(company.getSitePage().getUrl());
+
+                            List<Role> rolesList = company.getRoles();
+                            dos.writeInt(rolesList.size());
+                            for (Role role : rolesList) {
+                                writeLocalDate(dos, role.getDateStart());
+                                writeLocalDate(dos, role.getDateEnd());
+                                dos.writeUTF(role.getDescription());
+                                dos.writeUTF(role.getName());
+                            }
+                        }
+                        break;
+                }
+            }
         }
     }
 
@@ -34,8 +75,53 @@ public class DataSerializerStrategy implements SerializerStrategy {
                 String val = dis.readUTF();
                 resume.addContacts(contactType, val);
             }
-            //TODO implements Sections
+
+            size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                resume.addSections(sectionType, getSectionForRead(dis, sectionType));
+            }
             return resume;
+        }
+    }
+
+    private void writeLocalDate(DataOutputStream dos, LocalDate date) throws IOException {
+        dos.writeInt(date.getYear());
+        dos.writeInt(date.getMonth().getValue());
+    }
+
+    private LocalDate readLocalDate(DataInputStream dis) throws IOException {
+        return LocalDate.of(dis.readInt(), dis.readInt(), 1);
+    }
+
+    private Section getSectionForRead(DataInputStream dis, SectionType sectionType) throws IOException {
+        switch (sectionType) {
+            case PERSONAL:
+            case OBJECTIVE:
+                return new TextBoxSection(dis.readUTF());
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                int size = dis.readInt();
+                List<String> listSectionList = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    listSectionList.add(dis.readUTF());
+                }
+                return new ListSection(listSectionList);
+            case EXPERIENCE:
+            case EDUCATION:
+                size = dis.readInt();
+                List<Company> companyList = new ArrayList<>(size);
+                for (int i = 0; i < size; i++) {
+                    int length = dis.readInt();
+                    List<Role> roleList = new ArrayList<>(length);
+                    for (int j = 0; j < length; j++) {
+                        roleList.add(new Role(readLocalDate(dis), readLocalDate(dis), dis.readUTF(), dis.readUTF()));
+                    }
+                    companyList.add(new Company(new Site(dis.readUTF(), dis.readUTF()), roleList));
+                }
+                return new CompanySection(companyList);
+            default:
+                throw new IllegalStateException();
         }
     }
 }
