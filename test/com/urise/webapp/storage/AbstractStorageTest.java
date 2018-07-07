f24a6e5cf2b216d216e7f33d8f7aa401ef9ff69f
Nick_Jv <rnjv2015@yandex.ru>
2018-07-07 19:32:05 +0300

    add new data for Test

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -31,15 +31,19 @@ public abstract class AbstractStorageTest {
         RESUME_3 = new Resume(UUID_3, "B");
         RESUME_4 = new Resume(UUID_4, "D");
         RESUME_1.addContacts(ContactType.PHONE, "+999999999");
-        RESUME_2.addSections(SectionType.OBJECTIVE, new TextBoxSection("OBJECTIVE"));
-        RESUME_3.addSections(SectionType.PERSONAL, new TextBoxSection("PERSONAL"));
         RESUME_1.addSections(SectionType.ACHIEVEMENT, new ListSection("A1", "A2", "A3"));
+        RESUME_1.addSections(SectionType.EDUCATION, new CompanySection(new Company("ЛЭТИ", "URL",
+                new Role(2016, Month.JANUARY, 2018, Month.APRIL, "Бакалавр"))));
+
+        RESUME_2.addSections(SectionType.PERSONAL, new TextBoxSection("PERSONAL"));
+        RESUME_2.addSections(SectionType.OBJECTIVE, new TextBoxSection("OBJECTIVE"));
         RESUME_2.addSections(SectionType.QUALIFICATIONS, new ListSection("Q1", "Q2", "Q3"));
+
+        RESUME_3.addSections(SectionType.PERSONAL, new TextBoxSection("PERSONAL"));
         RESUME_3.addSections(SectionType.EXPERIENCE, new CompanySection(new Company("Title", "URL",
                 new Role(2016, Month.JANUARY, 2018, Month.APRIL, "Инженер", "Работал в лаборатории"),
                 new Role(2015, Month.JANUARY, 2017, Month.APRIL, "Инженер", "Работал в лаборатории"))));
-        RESUME_1.addSections(SectionType.EDUCATION, new CompanySection(new Company("ЛЭТИ", "URL",
-                new Role(2016, Month.JANUARY, 2018, Month.APRIL, "Бакалавр"))));
+
     }
 
     protected Storage storage;
