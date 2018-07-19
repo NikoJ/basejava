2641bb956637e7042f3dbd45877556eefd89f641
Nick_Jv <rnjv2015@yandex.ru>
2018-07-19 07:45:01 +0300

    added getStorage() -> return SqlStorage(db,user,pass)

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -1,5 +1,8 @@
 package com.urise.webapp;
 
+import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.storage.Storage;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,20 +12,15 @@ import java.util.Properties;
 public class Config {
     private static final File PROPERTIES_DIR = new File(".\\config\\resumes.properties");
     private static final Config INSTANCE = new Config();
-
-    private Properties properties = new Properties();
-    private File storageDir;
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
+    private final File storageDir;
+    private final Storage storage;
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPERTIES_DIR)) {
+            Properties properties = new Properties();
             properties.load(is);
             storageDir = new File(properties.getProperty("storage.dir"));
-            dbUrl = properties.getProperty("db.url");
-            dbUser = properties.getProperty("db.user");
-            dbPassword = properties.getProperty("db.password");
+            storage = new SqlStorage(properties.getProperty("db.url"), properties.getProperty("db.user"), properties.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPERTIES_DIR.getAbsolutePath());
         }
@@ -36,15 +34,7 @@ public class Config {
         return storageDir;
     }
 
-    public String getDbUrl() {
-        return dbUrl;
-    }
-
-    public String getDbUser() {
-        return dbUser;
-    }
-
-    public String getDbPassword() {
-        return dbPassword;
+    public Storage getStorage() {
+        return storage;
     }
 }
